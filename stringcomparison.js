14d17810e7c90ae78f937a414eda7900314c0939
esakki29 <esakkilekha@gmail.com>
2020-06-21 20:42:20 +0530

    Create stringcomparison.js

---
+++ stringcomparison.js
@@ -0,0 +1,6 @@
+/* what is the expected output for the below string comparison JS snippet  - Quiz */
+
+let name ="JS"
+let fullname == "Ecmascript"
+ if(name > fullname) console.log(name);
+ else{console.log(fullname)};
