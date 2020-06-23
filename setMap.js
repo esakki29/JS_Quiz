c3ce3188f0b249856151ac6ff754f66906243ba1
esakki29 <esakkilekha@gmail.com>
2020-06-23 21:03:42 +0530

    Create setMap.js

---
+++ setMap.js
@@ -0,0 +1,8 @@
+
+/* what is the expected output for the given code snipper - Js-Quiz*/
+let setMap = new Map();
+setMap.set('a','javascript');
+console.log(setMap.size);
+setMap.clear();
+setMap['b'] ='React'
+console.log(setMap.size);
